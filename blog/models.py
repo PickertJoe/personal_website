71a63c6e7e52e807e58c2ba0a252a6f4d249ae3a
Joseph Pickert <pickertjoe@gmail.com>
2019-06-16 16:38:12 -0600

    Filling out blog functionality

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    name=models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
 class Post(models.Model):
@@ -13,3 +16,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
