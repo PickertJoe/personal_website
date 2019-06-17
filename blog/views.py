52d49a14c74aa39ae2b71f2afee71a79fed61994
Joseph Pickert <pickertjoe@gmail.com>
2019-06-17 09:22:08 -0600

    Creating post templates for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from blog.models import Category, Post, Comment
-from . import CommentForm
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 # Create your views here.
 
 
 def blog_index(request):
-    posts = Post.objects.all.order_by('-created_on')
+    posts = Post.objects.all().order_by('-created_on')
     context = {'posts': posts}
     return render(request, 'blog_index.html', context)
 
