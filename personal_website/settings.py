446ba0506ac3b929d1c1d1cf72ed4d404fa309b9
Joseph Pickert <pickertjoe@gmail.com>
2019-06-18 09:28:34 -0600

    Localized bootstrap css files

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join("projects", "static"),
+)
+
 STATIC_URL = '/static/'
