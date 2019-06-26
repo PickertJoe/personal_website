eb7f9de87843359001ca3c101b05638d773df69b
Joseph Pickert <pickertjoe@gmail.com>
2019-06-26 13:03:07 -0600

    Added dropdown, collected static files

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'pagedown',
 
     # Personal Apps
     'projects',
@@ -128,3 +129,5 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
