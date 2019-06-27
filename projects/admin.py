82bbbdd800c00e9e984c9cb7d0a990b93252b8b1
Joseph Pickert <pickertjoe@gmail.com>
2019-06-27 11:59:40 -0600

    Collecting static, adding project admin functionality

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.db import models
+from projects.models import Project
+
+from pagedown.widgets import AdminPagedownWidget
+
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminPagedownWidget},
+    }
+
+
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -7,5 +7,5 @@ class Project(models.Model):
     """Used for displaying projects of interest on personal website"""
     title = models.CharField(max_length=200)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='/img')
+    technology = models.CharField(max_length=200)
+    image = models.FilePathField(path='static/images', default='/images/snow.jpg')
