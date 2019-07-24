d35a509aa56019adec13049defd17f5338918ae8
Joseph Pickert <pickertjoe@gmail.com>
2019-07-24 15:39:00 -0600

    Migrations and implimenting city model class

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,35 @@
+import requests
 from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 
 
 def weather_index(request):
-    return render(request, 'weather_index.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=1442deb6d63d49a6d94e45200a09fea7'
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        # Makes the API call and interprets the result in JSON format
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'low_temperature': r['main']['temp_min'],
+            'high_temperature': r['main']['temp_max'],
+            'humidity': r['main']['humidity'],
+            'description': r['weather'][0]['description'],
+            'wind': r['wind']['speed'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    print(weather_data)
+
+    context = {'weather_data': city_weather}
+    return render(request, 'weather_index.html', context)
