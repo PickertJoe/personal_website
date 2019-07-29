e614d398e624906848671f8b2c6ec09fdcf69bf9
Joseph Pickert <pickertjoe@gmail.com>
2019-07-29 11:19:09 -0600

    Implemented optional field for project model to link to live application

--- projects/models.py
+++ projects/models.py
@@ -10,6 +10,7 @@ class Project(models.Model):
     technology = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     link = models.URLField(default='')
+    app_link = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.title
